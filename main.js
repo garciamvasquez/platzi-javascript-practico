5b7bcf4655e9eaace410b8a708e81ad3bc91130b
Manuel Vasquez Garcia <garciamvasquez@gmail.com>
2023-12-29 20:05:44 -0500

    Integración mobile-menu y imagen menu hamburguesa al index.html

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let navbarEmail = document.querySelector('.navbar-email');
 let desktopMenu = document.querySelector('.desktop-menu');
+let imgMobileMenu = document.querySelector('.menu');
+let mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+imgMobileMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
